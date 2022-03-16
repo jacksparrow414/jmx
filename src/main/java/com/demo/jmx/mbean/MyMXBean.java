2a7112458c801634b5880b67121f960036aec1e2
Serendipity <jacksparrow414@163.com>
2022-03-16 09:00:05 +0800

    add demos and comments

--- src/main/java/com/demo/jmx/mbean/MyMXBean.java
+++ src/main/java/com/demo/jmx/mbean/MyMXBean.java
@@ -1,17 +1,29 @@
 package com.demo.jmx.mbean;
 
+import com.demo.jmx.entity.User;
+
 import javax.management.MXBean;
 
 /**
  * 两种方式
  * 1、使用注解
  * 2、接口名字以MXBean结尾
+ *
+ * 对于要取值的属性，要有get方法
+ *
+ * 对于设值的属性，要有set方法
+ *
+ * 对象无法设值，只能是基本类型和包装类
  */
 @MXBean
 public interface MyMXBean{
 
+    User getUser();
+
     String getName();
 
     void setName(String name);
+
+    void logMessage();
 
 }
--- src/main/java/com/demo/jmx/mbean/MyMXBeanImpl.java
+++ src/main/java/com/demo/jmx/mbean/MyMXBeanImpl.java
@@ -1,8 +1,33 @@
 package com.demo.jmx.mbean;
 
+import com.demo.jmx.entity.User;
+import lombok.extern.java.Log;
+import org.springframework.boot.admin.SpringApplicationAdminMXBean;
+
+import java.lang.management.MemoryMXBean;
+
+/**
+ * 给要在JMX中查看的属性赋初始值，否则在JXM中看不到
+ *
+ * JMX中可以看到属性、操作两个
+ *
+ * 属性对应的是在当前实现类中定义的，对属性的具体操作方法get/set则是在MXBean中定义的，二者通过接口中实现的方法进行关联
+ *
+ * 操作则是不涉及当前类中的属性
+ *
+ * 可参考{@link SpringApplicationAdminMXBean}和{@link MemoryMXBean}
+ */
+@Log
 public class MyMXBeanImpl implements MyMXBean {
 
     private String name = "jack";
+
+    private User user = User.builder().name("jack").password(888888L).used(false).build();;
+
+    @Override
+    public User getUser() {
+        return user;
+    }
 
     @Override
     public String getName() {
@@ -12,5 +37,13 @@ public class MyMXBeanImpl implements MyMXBean {
     @Override
     public void setName(String name) {
         this.name = name;
+    }
+
+    /**
+     * JMX 操作示例
+     */
+    @Override
+    public void logMessage() {
+        log.info("log message");
     }
 }
