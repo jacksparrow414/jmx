d043b250c20584bcac20cf5f09e3a174293e3008
Serendipity <jacksparrow414@163.com>
2022-03-16 09:19:52 +0800

    add comments

--- src/main/java/com/demo/jmx/mbean/MyMXBean.java
+++ src/main/java/com/demo/jmx/mbean/MyMXBean.java
@@ -14,6 +14,18 @@ import javax.management.MXBean;
  * 对于设值的属性，要有set方法
  *
  * 对象无法设值，只能是基本类型和包装类
+ *
+ * jconsole中看到的属性是方法名去掉get后的名字，如下面的getUser,getName,
+ * 可以尝试将getName和setName修改为getname、setname，再观察jconsole，发现属性由Name变为name了
+ *
+ * 官方文档原话 对应的文档： https://docs.oracle.com/javase/tutorial/jmx/mbeans/standard.html
+ * Getter and setter methods are declared to allow the managed application to access and possibly change the attribute values. As defined by the JMX specification,
+ * a getter is any public method that does not return void and whose name begins with get.
+ * A getter enables a manager to read the value of the attribute,
+ * whose type is that of the returned object.
+ * A setter is any public method that takes a single parameter and whose name begins with set.
+ * A setter enables a manager to write a new value in the attribute,
+ * whose type is the same as that of the paramete
  */
 @MXBean
 public interface MyMXBean{
